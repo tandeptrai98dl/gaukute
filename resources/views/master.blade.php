3c2e818baeb4e982ca4c54f25f5e7255e379dd83
Tan Nguyen <tandeptrai98dl@gmail.com>
2019-05-03 10:46:18 +0700

    Update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel </title>
     <base href="{{asset('')}}">
-    <link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="source/assets/dest/css/font-awesome.min.css">
     <link rel="stylesheet" href="source/assets/dest/vendors/colorbox/example3/colorbox.css">
     <link rel="stylesheet" href="source/assets/dest/rs-plugin/css/settings.css">
@@ -15,6 +12,7 @@
     <link rel="stylesheet" title="style" href="source/assets/dest/css/style.css">
     <link rel="stylesheet" href="source/assets/dest/css/animate.css">
     <link rel="stylesheet" title="style" href="source/assets/dest/css/custom-style.css">
+    <link rel="stylesheet" title="style" href="source/assets/dest/css/boostrap.min.css">
 </head>
 <body>
 
@@ -29,7 +27,6 @@
 <!-- include js files -->
 <script src="source/assets/dest/js/jquery.js"></script>
 <script src="source/assets/dest/vendors/jqueryui/jquery-ui-1.10.4.custom.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 <script src="source/assets/dest/vendors/bxslider/jquery.bxslider.min.js"></script>
 <script src="source/assets/dest/vendors/colorbox/jquery.colorbox-min.js"></script>
 <script src="source/assets/dest/vendors/animo/Animo.js"></script>
@@ -39,6 +36,7 @@
 <script src="source/assets/dest/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 <script src="source/assets/dest/js/waypoints.min.js"></script>
 <script src="source/assets/dest/js/wow.min.js"></script>
+<script src="source/assets/dest/js/bootstrap.min.js"></script>
 <!--customjs-->
 <script src="source/assets/dest/js/custom2.js"></script>
 <script>
